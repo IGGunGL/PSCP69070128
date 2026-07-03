0bb6314b6555dd373e99bc1244bc10c33762663c
IGGunGL <69070128@kmitl.ac.th>
2026-07-03 10:52:11 +0700

    asd

---
+++ 2981/main.py
@@ -0,0 +1,5 @@
+"""GL"""
+name = input()
+surname =input()
+print("Hello", name, surname)
+print(name[0:2]+surname[0:2])
---
+++ 2995/main.py
@@ -0,0 +1,7 @@
+""""GL"""
+idcard = input()
+itcard = idcard[2:4]
+if itcard == "16":
+    print("yes")
+else:
+    print("no")
